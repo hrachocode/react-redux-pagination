0b9176ffe717f2e0915b03ffc3291a8bfd7dbf71
hrachocode <hracho94@yahoo.com>
2020-01-14 02:50:23 +0400

    Updates

---
+++ precache-manifest.ea1d8903edb45e93de5c4819d8eefeea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b15311e2e78d3d47843b3d6564e056",
+    "url": "/react-redux-pagination/index.html"
+  },
+  {
+    "revision": "c14c195c0e40bed2841b",
+    "url": "/react-redux-pagination/static/css/main.0f9ab5f8.chunk.css"
+  },
+  {
+    "revision": "d4958d3c1c3741cad792",
+    "url": "/react-redux-pagination/static/js/2.5664466f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-redux-pagination/static/js/2.5664466f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c14c195c0e40bed2841b",
+    "url": "/react-redux-pagination/static/js/main.f266b07b.chunk.js"
+  },
+  {
+    "revision": "7b4fba02b4d02d909767",
+    "url": "/react-redux-pagination/static/js/runtime-main.2cf75323.js"
+  }
+]);
